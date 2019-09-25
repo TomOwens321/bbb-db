08d4a0d64371e198d5ff6b3cb2dc24a86bae6a5d
Tom Owens <tom@towens.com>
2019-09-25 17:31:27 -0600

    Almost - tests that doubles do exist

--- tests/models/test_relations.py
+++ tests/models/test_relations.py
@@ -13,3 +13,9 @@ def test_plant_location_relations(one_plant,one_location):
     one_plant.locations.append(one_location)
     assert 'Greenus' in one_location.plants[0].name
     assert 'Here' in one_plant.locations[0].name
+
+def test_double_relations(one_plant,one_bug):
+    one_plant.bugs.append(one_bug)
+    one_bug.plants.append(one_plant)
+    print(one_bug.plants)
+    assert len(one_bug.plants) == 2
