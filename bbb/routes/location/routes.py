006a006b0c6ed180eb65a6828c081cb6d8929216
Tom Owens <tom@towens.com>
2019-09-28 05:07:03 -0600

    Missed one

--- bbb/routes/location/routes.py
+++ bbb/routes/location/routes.py
@@ -4,7 +4,7 @@ from . import location
 from bbb.models import Location
 from bbb import db
 
-@seed.route('/location/')
+@location.route('/location/')
 def list_locations():
     all_locations = db.session.query(Location).all()
     return render_template('location/location.html', items=all_locations)
