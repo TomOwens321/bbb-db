caf8490d7a2b0bf58045277d8a3a8a7412b000f3
Tom Owens <tom@towens.com>
2019-09-29 06:09:59 -0600

    Form for Location

--- bbb/routes/location/routes.py
+++ bbb/routes/location/routes.py
@@ -1,8 +1,18 @@
 from flask import render_template, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, DecimalField
 from . import location
 from bbb.models import Location
 from bbb import db
+
+class ReusableForm(Form):
+    name = StringField('Name: ', validators=[validators.DataRequired()])
+    state = StringField('State: ')
+    city = StringField('City: ')
+    latitude = DecimalField('Latitude: ')
+    longitude = DecimalField('Longitude: ')
+    altitude = DecimalField('Altitude: ')
+    desc = TextAreaField('Description: ')
+
 
 @location.route('/location/')
 def list_locations():
