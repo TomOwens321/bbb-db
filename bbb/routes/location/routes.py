ed26b6c21fbb8f153f29579e9a9349defa864403
Tom Owens <tom@towens.com>
2019-09-29 06:56:54 -0600

    Location new/edit form

--- bbb/routes/location/routes.py
+++ bbb/routes/location/routes.py
@@ -18,3 +18,33 @@ class ReusableForm(Form):
 def list_locations():
     all_locations = db.session.query(Location).all()
     return render_template('location/location.html', items=all_locations)
+
+@location.route('/location/new/', methods=['GET', 'POST'])
+@location.route('/location/<int:id>/edit/', methods=['GET', 'POST'])
+def new_location(id=None):
+    if id:
+        location = db.session.query(Location).filter(Location.id == id).first()
+        form = ReusableForm(request.form, obj=location)
+    else:
+        location = Location()
+        form = ReusableForm(request.form)
+    print(form.errors)
+    if request.method == 'POST':
+        location.name = request.form['name']
+        location.state = request.form['state']
+        location.city = request.form['city']
+        location.latitude = request.form['latitude']
+        location.longitude = request.form['longitude']
+        location.altitude = request.form['altitude']
+        location.desc = request.form['desc']
+
+        if form.validate():
+            session = db.session()
+            session.add(location)
+            session.commit()
+            session.close()
+            flash('Saving Location')
+
+        else:
+            flash('Unable to save Location')
+    return render_template('location/form.html', form=form)
